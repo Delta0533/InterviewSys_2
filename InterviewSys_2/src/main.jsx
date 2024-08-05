6ddbbbc72e26ce0a90cd15dd20acfc05c3afa70e
Pearapat <138387155+PPraapatZZ@users.noreply.github.com>
2024-08-05 23:49:50 +0700

    Update main.jsx

--- InterviewSys_2/src/main.jsx
+++ InterviewSys_2/src/main.jsx
@@ -11,7 +11,7 @@ import LoginPage from "./pages/LoginPage/Login.jsx";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Login />,
+        element: <DefaultPage />,
     },
     {
         path: "/login",
